f6979ec35a359be068196df2530968e5206769ff
jvcataquiz <jvcataquiz@gmail.com>
2021-11-03 13:04:42 +0800

    13th Commit

--- ScientificCalculator/_Opening.Designer.cs
+++ ScientificCalculator/_Opening.Designer.cs
@@ -30,19 +30,22 @@ namespace ScientificCalculator
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(_Opening));
             this.loading = new System.Windows.Forms.ProgressBar();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.welcome = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // loading
             // 
-            this.loading.BackColor = System.Drawing.Color.White;
+            this.loading.BackColor = System.Drawing.Color.Black;
             this.loading.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.loading.Location = new System.Drawing.Point(25, 162);
+            this.loading.Location = new System.Drawing.Point(41, 242);
             this.loading.Name = "loading";
-            this.loading.Size = new System.Drawing.Size(593, 54);
+            this.loading.Size = new System.Drawing.Size(438, 34);
             this.loading.TabIndex = 0;
             this.loading.Click += new System.EventHandler(this.loading_Click);
             // 
@@ -59,26 +62,40 @@ namespace ScientificCalculator
             // welcome
             // 
             this.welcome.AutoSize = true;
-            this.welcome.Font = new System.Drawing.Font("Algerian", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.welcome.Location = new System.Drawing.Point(127, 51);
+            this.welcome.BackColor = System.Drawing.Color.Transparent;
+            this.welcome.Font = new System.Drawing.Font("Algerian", 70F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.welcome.Location = new System.Drawing.Point(28, 92);
+            this.welcome.Margin = new System.Windows.Forms.Padding(0);
             this.welcome.Name = "welcome";
-            this.welcome.Size = new System.Drawing.Size(401, 89);
+            this.welcome.Size = new System.Drawing.Size(468, 104);
             this.welcome.TabIndex = 3;
             this.welcome.Text = "WELCOME";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(525, 336);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
             // 
             // _Opening
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(643, 254);
+            this.ClientSize = new System.Drawing.Size(525, 336);
             this.ControlBox = false;
             this.Controls.Add(this.welcome);
             this.Controls.Add(this.loading);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "_Opening";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Opening";
             this.Load += new System.EventHandler(this._Opening_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,5 +106,6 @@ namespace ScientificCalculator
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Timer timer2;
         private System.Windows.Forms.Label welcome;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- ScientificCalculator/_Opening.cs
+++ ScientificCalculator/_Opening.cs
@@ -21,18 +21,7 @@ namespace ScientificCalculator
 
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-           
-            loading.Value += 1;
-            if (loading.Value == 100)
-            {
-                timer1.Stop();
-                this.Hide();
-                Calcu calcunew = new Calcu();
-                calcunew.Show();
-            }
-        }
+        
 
         public string welcomestr;
         public int count;
@@ -43,16 +32,28 @@ namespace ScientificCalculator
             welcomestr = "WELCOME";
             timer2.Enabled = true;
         }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
 
+            loading.Value += 1;
+            if (loading.Value == 100)
+            {
+                timer1.Stop();
+                this.Hide();
+                Calcu calcunew = new Calcu();
+                calcunew.Show();
+            }
+        }
         private void timer2_Tick(object sender, EventArgs e)
         {
+            welcome.Text = welcomestr.Substring(0, count);
+            count++;
             if (welcome.Text.Length == welcomestr.Length)
             {
                 timer2.Enabled = false;
                 return;
             }
-            welcome.Text = welcomestr.Substring(0, count);
-            count++;
+           
         }
 
     }
